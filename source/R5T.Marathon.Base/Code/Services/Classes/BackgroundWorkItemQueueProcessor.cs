8f496b4163470fa009c9107edcdc2376dcb2748d
MinexAutomation <minex.automation@gmail.com>
2019-11-22 17:28:23 -0800

    Added background work item queue and processor.

---
+++ source/R5T.Marathon.Base/Code/Services/Classes/BackgroundWorkItemQueueProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.Marathon
+{
+    /// <summary>
+    /// Based on QueuedHostedService from: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-2.2&tabs=visual-studio#queued-background-tasks-1
+    /// </summary>
+    public class BackgroundWorkItemQueueProcessor : BackgroundService
+    {
+        private IBackgroundWorkItemQueue BackgroundWorkItemQueue { get; }
+        private ILogger Logger { get; }
+
+
+        public BackgroundWorkItemQueueProcessor(IBackgroundWorkItemQueue backgroundWorkItemQueue, ILogger<BackgroundWorkItemQueueProcessor> logger)
+        {
+            this.BackgroundWorkItemQueue = backgroundWorkItemQueue;
+            this.Logger = logger;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            this.Logger.LogInformation("Queued Hosted Service is starting.");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var workItem = await this.BackgroundWorkItemQueue.DequeueAsync(cancellationToken);
+
+                try
+                {
+                    await workItem(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.LogError(ex, $"Error occurred executing {nameof(workItem)}.");
+                }
+            }
+
+            this.Logger.LogInformation("Queued Hosted Service is stopping.");
+        }
+    }
+}
---
+++ source/R5T.Marathon.Base/Code/Services/Interfaces/IBackgroundWorkItemQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace R5T.Marathon
+{
+    public interface IBackgroundWorkItemQueue
+    {
+        void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem);
+        Task<Func<CancellationToken, Task>> DequeueAsync(CancellationToken cancellationToken);
+    }
+}
