cf55c0ffe7549e224ef3b0cb4bdda1b3edd10ad7
David Coats <david@getsafetycone.com>
2020-08-21 01:05:19 -0700

    Add IBackgroundWorkItemQueueExtensions

---
+++ source/R5T.Marathon.Base/Code/Services/Extensions/IBackgroundWorkItemQueueExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace R5T.Marathon
+{
+    public static class IBackgroundWorkItemQueueExtensions
+    {
+        public static void Enqueue(this IBackgroundWorkItemQueue backgroundWorkItemQueue, Func<IServiceProvider, CancellationToken, Task> workItem)
+        {
+            backgroundWorkItemQueue.QueueBackgroundWorkItem(workItem);
+        }
+    }
+}
